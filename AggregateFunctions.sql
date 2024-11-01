2491d0d7c4dfd223db886ae7ce905ec5954c4386
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-11-01 22:53:20 +0530

    Create AggregateFunctions.sql

---
+++ AggregateFunctions.sql
@@ -0,0 +1,9 @@
+select min(price) as pricee from customers
+
+select max(price) as pricee from customers
+
+select count(cust_name) as totalcustomers from customers
+
+select avg(price) as Average from customers
+
+select sum(price) as totalPrice from customers
